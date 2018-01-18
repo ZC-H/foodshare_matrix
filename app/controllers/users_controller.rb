b7f8c2c04c79dac9f7efbba34434e25b98cf5c13
limchiahooi <limchiahooi@gmail.com>
2018-01-18 19:57:29 +0800

    Users routes, controller and profile
    1. Added resources :users in routes file
    2. Added users_controller file
    3. Added user profile html and css files
    4. Added index html and css files

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,61 @@
+class UsersController < Clearance::UsersController
+
+
+	def index
+		@user = User.all
+	end
+
+	def new
+		@user = User.new
+		render 'users/new'
+	end
+
+	def create
+		@user = User.new(user_from_params)
+
+		if @user.save
+			sign_in @user
+			flash[:success] = "Welcome"
+			redirect_to root_url
+		else 
+			flash[:danger] = @user.errors.full_messages.first
+			
+			redirect_to sign_up_path
+			
+		end
+	end
+
+	
+
+
+	def show
+		@user = User.find(params[:id])
+		
+		render 'users/show'
+	end
+
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+
+	def update
+		@user = User.find(params[:id])
+		@user.update_attributes(user_from_params)
+		redirect_to @user
+	end 
+
+
+
+    def user_from_params
+		params.require(:user).permit(:email, :password, :avatar, :remove_avatar)
+	end
+
+
+
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,16 @@ Rails.application.routes.draw do
       only: [:create, :edit, :update]
   end
 
+
+
+  resources :users
+
+
+
+
+
+
+
   get "/sign_in" => "clearance/sessions#new", as: "sign_in"
   delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
   get "/sign_up" => "clearance/users#new", as: "sign_up"
