0aab9d536d3617912f0270e3d229d25f215d311a
ZC-H <cern.hoh@gmail.com>
2018-01-18 14:46:26 +0800

    Added basic event creation form and show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,44 @@
-class EventController < ApplicationController
+class EventsController < ApplicationController
+	before_action	:find_event, only: [:show, :edit, :update]
+	def find_event
+		@event = Event.find(params[:id])
+	end
+
+	def index
+	end
+
+	def new
+		@event = Event.new
+	end
+
+	def create
+		@event = current_user.events.new(event_params)
+		@event.location_id = 1 #Edit this later
+		p "ccccccccccc"
+		p @event
+		if @event.save
+		  redirect_to @event
+		else
+			p "aaaaaaaaaaaaaaaa"
+			p @errors
+		  render 'new'
+
+		  #Flash errors here
+		end
+	end
+
+	def show
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	private
+	def event_params
+	  params.require(:event).permit(:name, :description, :expected, :food_amount)
+	end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180118055324) do
+ActiveRecord::Schema.define(version: 20180118062045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20180118055324) do
     t.time "end_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "description"
     t.index ["location_id"], name: "index_events_on_location_id"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
