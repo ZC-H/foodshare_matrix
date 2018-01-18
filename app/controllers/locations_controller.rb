d7121e5e6ec8ced6014494245c37771bf313974e
ZC-H <cern.hoh@gmail.com>
2018-01-18 17:55:18 +0800

    Can create locations by address and view them on a static map

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,33 @@
+class LocationsController < ApplicationController
+	before_action	:find_location, only: [:show, :edit, :update]
+	def find_location
+		@location = Location.find_by_id(params[:id])
+		if !@location
+			#Put a real error page here later
+			redirect_to '/'
+		end
+	end
+
+	def show
+	end
+
+	def new
+		@location = Location.new
+	end
+
+	def create
+    @location = Location.new(location_params)
+    if @location.save
+      flash[:success] = "Place added!"
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def location_params
+      params.require(:location).permit(:name, :address)
+    end
+end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ApplicationRecord
 	has_many	:events
-	
+	geocoded_by :address
+	after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 end
