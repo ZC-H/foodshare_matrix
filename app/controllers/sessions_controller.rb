177ce220966ab5ca6d8aa3fac2f76452fb1f337f
ZC-H <cern.hoh@gmail.com>
2018-01-19 15:17:49 +0800

    Almost fixed sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,19 @@ class SessionsController < Clearance::SessionsController
 		@user = User.new
 	end
 
+
+	def create
+	  @user = authenticate(params)
+
+	  sign_in(@user) do |status|
+	    if status.success?
+	      redirect_back_or url_after_create
+	    else
+	      flash.now.notice = status.failure_message
+	      redirect '/sign_in', status: :unauthorized
+	    end
+	  end
+	end
+
+
 end
